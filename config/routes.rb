53bdd380a3c240a3edfe199fb3b9f910578ceba9
tharon <tharonweighill@gmail.com>
2021-03-24 11:19:22 -0600

    updated resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root 'home#signup'
-  resources :restaurant
-  resources :gm
-  resources :agm
-  resources :chef
+  
+  resources :restaurant, only: [:new, :show] do
+
+  #super admin
+  resources :gm do 
+    resources :kitchen_notes
+    resources :dining_notes
+  end
+  
+  #admins
+  resources :agm do
+    resources :dining_preps
+  end 
+  resources :chef do
+    resources :kitchen_preps
+  end
+
+  #users
   resources :cook
   resources :server
-  resources :kitchen_notes
-  resources :kitchen_preps
-  resources :dining_notes
-  resources :dining_preps
+  
 end

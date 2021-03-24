dae4d1267c929c9735ced861810453bb13a033db
tharon <tharonweighill@gmail.com>
2021-03-24 11:01:43 -0600

    created resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#signup'
-  get 'gm'
+  resources :restaurant
+  resources :gm
+  resources :agm
+  resources :chef
+  resources :cook
+  resources :server
+  resources :kitchen_notes
+  resources :kitchen_preps
+  resources :dining_notes
+  resources :dining_preps
 end

a4b2f403f039714668c1bc86f8122ba15facb13e
tharon <tharonweighill@gmail.com>
2021-04-14 10:15:53 -0600

    fixed db issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
-  
+  before_action :redirect_if_logged_in, only: [:new]
+
+  def new
+    @bartender = Bartender.new
+  end
 
   def create
     @bartender = Bartender.find_by(username: params[:username])
@@ -10,5 +14,26 @@ class SessionsController < ApplicationController
       redirect_to '/recipes'
     end
   end 
+
+  def omniauth
+    # use byebug to inspect what the auth method returns
+    @bartender = Bartender.from_omniauth(auth)
+    if @bartender.valid?
+      session[:bartender_id] = @bartender.id
+      redirect_to @bartender
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to "/"
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
- 
